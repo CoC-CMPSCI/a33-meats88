9fd90ade54c6b5affbe0d7732406277997d2d673
meats88 <pcx883mi@gmail.com>
2026-03-24 19:02:23 -0700

    done

--- main.cpp
+++ main.cpp
@@ -4,15 +4,17 @@ using namespace std;
 
 int main()
 {
-    double C;
+    double T;
     double F;
 
     // TODO: [Step 1] Input a Celcius degree as a "double" type value
-
+cout << "give temp in c";
+cin >> T;
+F = (9/5.0) * T + 32;
     // TODO: [Step 2] Convert it to a Fahrenheit degree using the Formula F = (9 / 5.0)* C + 32
 
     cout << setprecision(2) << fixed;
     // TODO: [Step 3] Print the converted Fahrenheit
-
+cout << F;
     return 0;
 }
